1c0773f1236c15078657915aebef3c58d40910e7
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-02-17 04:19:41 +0600

    balance

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.getElementById("calculate-btn").addEventListener("click", function () {
+  let income = document.getElementById("monthly-income").value;
   let foodCost = document.getElementById("food-cost").value;
   let rentCost = document.getElementById("rent-cost").value;
   let pocketCost = document.getElementById("pocket-cost").value;
   let totalCost = Number(foodCost) + Number(rentCost) + Number(pocketCost);
+  let expenses = document.getElementById("total");
+  expenses.innerText = totalCost;
+  let remainingBalance = Number(income) - totalCost;
+  let balance = document.getElementById("remaining-balance");
+  balance.innerText = remainingBalance;
 });
