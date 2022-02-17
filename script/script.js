6b62a9f83db173dc4ec3d8e1038b13e7c26399e0
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-02-17 20:54:00 +0600

    saving

--- script/script.js
+++ script/script.js
@@ -1,33 +1,48 @@
-// Calculation Button
-document.getElementById("calculate-btn").addEventListener("click", function () {
+// Calculation Section
+document
+  .getElementById("calculate-button")
+  .addEventListener("click", function () {
+    let income = document.getElementById("monthly-income").value;
+    if (income < 0 || isNaN(income)) {
+      return alert(
+        '❌Error in INCOME field❌: Please Fill with "Positive Number"'
+      );
+    }
+    let foodCost = document.getElementById("food-cost").value;
+    if (foodCost < 0 || isNaN(foodCost)) {
+      return alert(
+        '❌Error in FOOD field❌: Please Fill with "Positive Number"'
+      );
+    }
+    let rentCost = document.getElementById("rent-cost").value;
+    if (rentCost < 0 || isNaN(rentCost)) {
+      return alert(
+        '❌Error in RENT field❌: Please Fill with "Positive Number"'
+      );
+    }
+    let pocketCost = document.getElementById("pocket-cost").value;
+    if (pocketCost < 0 || isNaN(pocketCost)) {
+      return alert(
+        '❌Error in POCKETCOST field❌: Please Fill with "Positive Number"'
+      );
+    }
+    let totalCost = Number(foodCost) + Number(rentCost) + Number(pocketCost);
+    let expenses = document.getElementById("total");
+    expenses.innerText = totalCost;
+    let remainingBalance = Number(income) - totalCost;
+    let balance = document.getElementById("total-balance");
+    balance.innerText = remainingBalance;
+  });
+
+// Saving Section
+document.getElementById("save-button").addEventListener("click", function () {
   let income = document.getElementById("monthly-income").value;
-  if (income < 0 || isNaN(income)) {
-    return alert(
-      '❌Error in INCOME field❌: Please Fill with "Positive Number"'
-    );
-  }
-  let foodCost = document.getElementById("food-cost").value;
-  if (foodCost < 0 || isNaN(foodCost)) {
-    return alert(
-      '❌Error in FOOD field❌: Please Fill with "Positive Number"'
-    );
-  }
-  let rentCost = document.getElementById("rent-cost").value;
-  if (rentCost < 0 || isNaN(rentCost)) {
-    return alert(
-      '❌Error in RENT field❌: Please Fill with "Positive Number"'
-    );
-  }
-  let pocketCost = document.getElementById("pocket-cost").value;
-  if (pocketCost < 0 || isNaN(pocketCost)) {
-    return alert(
-      '❌Error in POCKETCOST field❌: Please Fill with "Positive Number"'
-    );
-  }
-  let totalCost = Number(foodCost) + Number(rentCost) + Number(pocketCost);
-  let expenses = document.getElementById("total");
-  expenses.innerText = totalCost;
-  let remainingBalance = Number(income) - totalCost;
-  let balance = document.getElementById("remaining-balance");
-  balance.innerText = remainingBalance;
+  let save = document.getElementById("saving").value;
+  let saveCalculation = (Number(income) * Number(save)) / 100;
+  let saveAmount = document.getElementById("save-amount");
+  saveAmount.innerText = saveCalculation;
+  let totalBalance = document.getElementById("total-balance");
+  let balance = Number(totalBalance.innerText) - Number(saveAmount.innerText);
+  let remainingBalance = document.getElementById("remain-balance");
+  remainingBalance.innerText = balance;
 });
